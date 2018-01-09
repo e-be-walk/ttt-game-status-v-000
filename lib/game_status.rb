e62edd5baec183d785bdd50311ad349fdf3f2d14
Erin Walker <erinb0317@hotmail.com>
2018-01-09 16:03:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,9 +63,8 @@ def over?(board)
 end
 
 def winner(board)
-  if !won(board)
-    return nil
-  else WIN_COMBINATIONS.each do |win_combination|
+  WIN_COMBINATIONS.each do |win_combination|
+  
     if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
       return "X"
     elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
